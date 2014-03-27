9e6028f35d675470ac36bb64765c2fd1f54ffac1
hohuynhtrinh <trinhmthhuynh@gmail.com>
2014-03-27 21:31:48 +0700

    Hoan thanh menu game, thieu ten game.

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public Vector3 scale = Vector3.one;
+	public int nextLevel = -1;
+	private Vector3 sca;
+	// Use this for initialization
+	void Start () {
+		sca = transform.lossyScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetMouseButtonDown(0))
+			//OnMouseDown();
+	}
+
+	void OnMouseDown() {
+		this.transform.localScale = scale;
+	}
+
+	void OnMouseUp() {
+		this.transform.localScale = sca;
+		if (nextLevel >= 0) {
+			Application.LoadLevel (nextLevel);
+		}
+	}
+}
---
+++ Assets/Script/SaoBang.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaoBang : MonoBehaviour {
+
+	Vector2 position;
+	public Vector2 speed = Vector2.zero;
+	public float len = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-speed.x, -speed.y, 0);
+		if (position.x - transform.position.x >= len) {
+			transform.position = position;		
+		}
+	}
+}
